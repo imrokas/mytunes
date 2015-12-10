4735745f1959e514fb4f56a28f2f3c46d4d881bf
imrokas <rokas@imrokas.com>
2015-12-09 19:24:01 -0500

    implemented songqueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,25 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add',function(song){
+      if(this.length === 1) {
+        this.playFirst();
+      }
+    }, this)
+    this.on('ended', function(x){
+      this.remove(this.at(0));
+      if(this.length > 0){
+        this.playFirst();
+      } 
+    })
+    this.on('dequeue', function(song){
+      console.log('Im in songQueue')
+      this.remove(song);
+    })
+  },
+
+  playFirst:function(){
+    this.at(0).play();
   }
 
 });
--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -5,6 +5,14 @@ var AppModel = Backbone.Model.extend({
     this.set('currentSong', new SongModel());
     this.set('songQueue', new SongQueue());
 
+    params.library.on('enqueue', function(song){
+      this.get('songQueue').add(song);
+    }, this);
+
+    // this.get('songQueue').on('dequeue', function(song){
+    //   console.log('i got dequeued')
+    //   this.get('songQueue').remove(song);
+    // }, this)
     /* Note that 'this' is passed as the third argument. That third argument is
     the context. The 'play' handler will always be bound to that context we pass in.
     In this example, we're binding it to the App. This is helpful because otherwise
